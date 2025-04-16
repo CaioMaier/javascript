1c096305e1f03e62efb315d51841387475cbc058
CaioMaier <caiomaier.work@gmail.com>
2025-04-16 15:07:49 -0300

    commit

---
+++ Curso-JS-udemy/aula009-tipos-primitivos/adendo.js
@@ -0,0 +1,6 @@
+// ECMAScript padronizacao do javascript criada em 2015 pela empresa ecma
+var nome = "Caio";
+var nome = "Jonas";
+let nome2 = "caio"
+nome2 = "caio2"
+console.log(nome2);
---
+++ Curso-JS-udemy/aula009-tipos-primitivos/index.js
@@ -0,0 +1,27 @@
+// TIPOS PRIMITIDOS
+
+/* STRINGS
+Tudo o que esta entre aspas ou craze.
+*/
+// Exemplos:
+
+const nome = "Caio";
+let name = `Nicole`;
+let filha = 'Helena';
+
+let nomeAluno; // Variavel declarada, mas sem valor "undefined"
+let sobrenomeAluno = null; // Nulo, utilizado QUEREMOS PROPOSITALMENTE, EXPLICITAMENTE remover o valor da variavel
+
+/* NUMBER
+Sao numeros, nao podem estrar entre aspas ou crazes, se nao se tornam strings */
+// EXEMPLOS:
+let idade = 23;
+
+/*BOOLEAN
+Contem duas opcoes de valores apenas: true ou false
+ */
+
+//EXEMPLO:
+
+const booleano = true;
+const boooleano = false;
