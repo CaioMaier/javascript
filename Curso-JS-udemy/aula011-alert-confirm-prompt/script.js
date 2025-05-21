fe07504fdccba6417d5de29804187264619d2dfc
Caio Maier <caiomaier.work@gmail.com>
2025-05-21 19:17:44 -0300

    commit

---
+++ Curso-JS-udemy/aula011-alert-confirm-prompt/script.js
@@ -0,0 +1,6 @@
+let num1 = prompt('Digite um número:');
+let num2 = prompt('Digite outro número:');
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+const soma = num1 + num2;
+window.alert(`A soma é: ${soma}`);
